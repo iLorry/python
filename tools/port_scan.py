0ddf49f2058f93f55bb8a1da4cec3d6e016c78bb
Lorry <cclorry@gmail.com>
2017-05-16 01:51:37 +0800

    feat: 扫描目标 IP 段的端口
    并发对目标 IP 段的若干个端口进行扫描，并返回扫描结果。默认返回 OPEN 的端口，
    如果带参数 --verbose 则返回所有扫描的 IP:PORT 及其 OPEN/CLOSE 状态。

---
+++ tools/port_scan.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+'''
+#=============================================================================
+#     FileName: port_scan.py
+#         Desc:
+#       Author: Lorry
+#        Email: cclorry@gmail.com
+#     HomePage:
+#      Version: 0.0.1
+#   LastChange: 2017-05-16 01:50:28
+#      History:
+#=============================================================================
+
+'''
+
+from socket import *
+import threading
+import argparse
+
+__version__ = '0.0.1'
+__all__ = ['port_scanner', 'main']
+
+lock = threading.Lock()
+threads = []
+
+def port_scanner(host: str, port: int, verbose: int =0) -> print:
+    '''
+    Scan the hosts's port and print the results.
+    '''
+
+    try:
+        s = socket(AF_INET, SOCK_STREAM)
+        s.connect((host, port))
+        lock.acquire()
+        if verbose < 1:
+            print('%s:%d' % (host, port))
+        else:
+            print('[OPEN] %s:%d' % (host, port))
+        lock.release()
+        s.close()
+    except:
+        if verbose < 1:
+            pass
+        else:
+            print('[CLOSE] %s:%d' % (host, port))
+
+def main():
+    '''
+    接收参数，并发请求 port_scanner
+    '''
+
+    parser = argparse.ArgumentParser(description='Scan the hosts\'s port.', prefix_chars='-/')
+
+    parser.add_argument('-H', '--hosts', dest='hosts', default='192.168.1.', type=str, help='The IP segment.', metavar='192.168.1.')
+    parser.add_argument('-b', '--begin', dest='begin', default=1, type=int, help='The begin of the IP segment.', metavar='1')
+    parser.add_argument('-e', '--end', dest='end', default=None, type=int, help='The end of the IP segment.', metavar='254')
+    parser.add_argument('-p', '--ports', dest='ports', default='80,443', type=str, help='Target ports.', metavar='80,443')
+    parser.add_argument('-v', '--verbose', dest='verbose', default=0, action='count', help='Verbose mode.')
+    parser.add_argument('-V', '--version', action='version', version='%(prog)s ' + __version__, help='Show the version number and exit.')
+
+    args = parser.parse_args()
+    ports = args.ports.split(',')
+
+    if (args.end == None) or (args.end < args.begin):
+        args.end = args.begin
+
+    setdefaulttimeout(1)
+
+    while args.begin <= args.end:
+
+        for port in ports:
+            host = '%s%s' % (args.hosts, args.begin)
+            t = threading.Thread(target=port_scanner, args=(host, int(port), args.verbose))
+            t.start()
+
+        for t in threads:
+            t.join()
+
+        args.begin += 1
+
+if __name__ == '__main__':
+    main()
+
+# demo: __name__ --hosts=192.168.1. --begin=1 --end=255 --ports=80,443 -v
+
+# vim: noai:ts=4:sw=4
